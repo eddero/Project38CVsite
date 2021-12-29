d90eed0cf349da0f1691416b7257e4b86f31705f
eddero <edvin97johansson@hotmail.com>
2021-12-29 13:14:25 +0100

    projectstable

--- Project38CVsite/Models/IdentityModels.cs
+++ Project38CVsite/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -10,6 +12,8 @@ namespace Project38CVsite.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Key]
+        public override string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
@@ -17,6 +21,9 @@ namespace Project38CVsite.Models
         public string Skill { get; set; }
         public string Experience { get; set; }
         public bool IsPrivate { get; set; }
+
+        [Required]
+        public virtual ICollection<Project> Projects { get; set; }
 
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
@@ -30,6 +37,7 @@ namespace Project38CVsite.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Project> projects { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -40,6 +48,7 @@ namespace Project38CVsite.Models
         {
             return new ApplicationDbContext();
         }
+
 
     }
 }
